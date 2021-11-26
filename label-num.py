a57dd33bb3a8bab75c582580ebd8a73ded46f558
Benjamin Thurber <bgt26@uclive.ac.nz>
2021-11-26 20:15:14 +1300

    Able to enter a starting number, and all matched numbers in the document will be added to the starting number and the sum will be replaced in the document.  Removed unused function.

--- label-num.py
+++ label-num.py
@@ -3,63 +3,8 @@ from docx import Document
 
 
 
-def paragraph_replace_text(paragraph, regex, replace_str):
-    """Return `paragraph` after replacing all matches for `regex` with `replace_str`.
-
-    `regex` is a compiled regular expression prepared with `re.compile(pattern)`
-    according to the Python library documentation for the `re` module.
-    """
-    # --- a paragraph may contain more than one match, loop until all are replaced ---
-    while True:
-        
-        text = paragraph.text
-        match = regex.search(text)
-        if not match:
-            break
-
-        # --- when there's a match, we need to modify run.text for each run that
-        # --- contains any part of the match-string.
-        runs = iter(paragraph.runs)
-        start, end = match.start(), match.end()
-
-        # --- Skip over any leading runs that do not contain the match ---
-        for run in runs:
-            run_len = len(run.text)
-            if start < run_len:
-                break
-            start, end = start - run_len, end - run_len
-
-        # --- Match starts somewhere in the current run. Replace match-str prefix
-        # --- occurring in this run with entire replacement str.
-        run_text = run.text
-        run_len = len(run_text)
-        run.text = "%s%s%s" % (run_text[:start], replace_str, run_text[end:])
-        end -= run_len  # --- note this is run-len before replacement ---
-
-        # --- Remove any suffix of match word that occurs in following runs. Note that
-        # --- such a suffix will always begin at the first character of the run. Also
-        # --- note a suffix can span one or more entire following runs.
-        for run in runs:  # --- next and remaining runs, uses same iterator ---
-            if end <= 0:
-                break
-            run_text = run.text
-            run_len = len(run_text)
-            run.text = run_text[end:]
-            end -= run_len
-
-    # --- optionally get rid of any "spanned" runs that are now empty. This
-    # --- could potentially delete things like inline pictures, so use your judgement.
-    # for run in paragraph.runs:
-    #     if run.text == "":
-    #         r = run._r
-    #         r.getparent().remove(r)
-
-    return paragraph
-
-
-
-def paragraph_replace_text(paragraph, regex, replace_str):
-    """Return `paragraph` after replacing all matches for `regex` with `replace_str`.
+def paragraph_replace_text(paragraph, regex, start_num, opening_char, closing_char):
+    """Return `paragraph` after replacing all matches for `regex` with `start_num`.
 
     `regex` is a compiled regular expression prepared with `re.compile(pattern)`
     according to the Python library documentation for the `re` module.
@@ -68,8 +13,10 @@ def paragraph_replace_text(paragraph, regex, replace_str):
     count = 0
     # --- a paragraph may contain more than one match, loop until all are replaced ---
     for match in regex.finditer(paragraph.text):
+        match_num = int(match.group(0).lstrip(opening_char).rstrip(closing_char))
+        label_num = start_num + match_num
         # --- calculate how much characters must be shifted to fix the match ---
-        padding = (len(replace_str) - (match.end() -match.start()) ) *count
+        padding = (len(str(label_num)) - (match.end() -match.start()) ) *count
 
         # --- when there's a match, we need to modify run.text for each run that
         # --- contains any part of the match-string.
@@ -87,7 +34,7 @@ def paragraph_replace_text(paragraph, regex, replace_str):
         # --- occurring in this run with entire replacement str.
         run_text = run.text
         run_len = len(run_text)
-        run.text = "%s%s%s" % (run_text[:start], replace_str, run_text[end:])
+        run.text = "%s%s%s" % (run_text[:start], str(label_num), run_text[end:])
         end -= run_len  # --- note this is run-len before replacement ---
 
         # --- Remove any suffix of match word that occurs in following runs. Note that
@@ -127,7 +74,7 @@ if __name__ == "__main__":
     start_num = int(input("Enter the label number to start at: "))
     
     for paragraph in document.paragraphs:
-        paragraph = paragraph_replace_text(paragraph, regex, "new_str")
+        paragraph = paragraph_replace_text(paragraph, regex, start_num, '<', '>')
     
     
     document.save('test_modified.docx')
